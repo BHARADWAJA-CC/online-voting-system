ed609367e80ef7b06abf2f4b61a81cf6b76971a2
BHARADWAJA-CC <24je0602@iitism.ac.in>
2025-12-29 19:42:54 +0530

    Update App.js

--- frontend/App.js
+++ frontend/App.js
@@ -1,1 +1,9 @@
-
+function vote(candidate) {
+  fetch("http://localhost:5000/vote", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ user: "test_user", candidate: candidate })
+  })
+  .then(res => res.json())
+  .then(data => alert(data.message));
+}
